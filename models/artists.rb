9be63dab7aa5484c6ab7f4c655cecb85ec5ac212
Andrew Samtoy <asamtoy@gmail.com>
2017-08-16 14:33:12 +0100

    adding at end of first pomodoro

---
+++ models/artists.rb
@@ -0,0 +1,16 @@
+require('pg')
+require_relative('../db/music')
+
+class Artists
+  attr_accessor(:name)
+
+  def initialize(artist_data)
+    @id = artist_data(['id'])
+    @name = artist_data['name']
+  end
+
+
+
+
+
+end
